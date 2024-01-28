03d461ba766e9df0297729ee844864d8737641b3
KugelblitzX <usage.avocado-0e@icloud.com>
2024-01-28 17:22:55 +0200

    done

--- task_2.py
+++ task_2.py
@@ -1,17 +1,19 @@
 import random
 
 def get_numbers_ticket(min, max, quantity):
-    if min >= 1 and max <= 1000:
-        population = range(min, max+1)
-        lot_numb = random.sample(population, quantity)
-        lot_numb.sort()
-
-        return lot_numb
-    else:
+    if min < 1 or max > 1000 or max - min + 1 < quantity or quantity < 0:
         print(f"Введіть значення в діапазоні від 1-1000")
+        return []
+    
+    population = range(min, max+1)
+    lot_numb = random.sample(population, quantity)
+    lot_numb.sort()
+
+    return lot_numb
+ 
 
 
-res = get_numbers_ticket(1, 1000, 1001)
+res = get_numbers_ticket(1, 1000, 10)
 print("Ваші лотерейні числа:", res)
 
 
