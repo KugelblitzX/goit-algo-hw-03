517c1e09911849e40fdb1221a7db9694a9389409
KugelblitzX <usage.avocado-0e@icloud.com>
2024-01-28 22:12:37 +0200

    done

--- task_3.py
+++ task_3.py
@@ -1,24 +1,21 @@
 import re
 
 def normalize_phone(phone_number):
-    f_numb = re.sub(r"[^\d+]", "", phone_number)
-    
-    if not f_numb.startswith("+"):
-        if not f_numb.startswith("380"):
-            f_numb = "+38" + f_numb
-        else:
-            f_numb = "+" + f_numb
-            
-    return f_numb
-
+    regex = r"[\d\+]+"
+    phone_number = "".join(re.findall(regex, phone_number))
+    if len(phone_number) == 10:
+        phone_number = "+38" + phone_number
+    elif len(phone_number) == 12:
+        phone_number = "+" + phone_number
+    return phone_number
 
 raw_numbers = [
     "    +38(050)123-32-34",
     "     0503451234",
     "(050)8889900",
     "38050-111-22-22",
-    "38050 111 22 11   "
+    "38050 111 22 11   ",
+    "432 11 222 22 22"
 ]
-
-sanitized_numbers = [normalize_phone(num) for num in raw_numbers]
-print("Нормалізовані номери телефонів для SMS-розсилки:", sanitized_numbers)
+for phone in raw_numbers:
+    print(normalize_phone(phone))
